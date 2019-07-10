2345ae059fa76d2875faa3efe8867686ad121395
DESKTOP-NDUN9JV\Nadya <nadia_degtiareva@rambler.ru>
2019-07-11 00:39:56 +0300

    vue components

---
+++ vue-0627/src/router.js
@@ -0,0 +1,20 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
+import About from "./views/About.vue";
+import 'bootstrap/scss/bootstrap.scss'
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: "/",
+      component: Home
+    },
+    {
+      path: "/about",
+      component: About
+    }
+  ]
+});
